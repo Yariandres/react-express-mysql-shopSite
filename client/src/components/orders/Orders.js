f203258db31f9f0ad9a01e317e781ddcfe787867
Yari Andres <yari.andres@gmail.com>
2020-11-08 01:31:58 +0100

    fetching products

--- client/src/components/orders/Orders.js
+++ client/src/components/orders/Orders.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 export const Orders = () => {
     return (
         <div className="orders">
-            <h1>This is the ORDERS component</h1>
+            <Container>
+                <h1>Hello from Orders component</h1>
+            </Container>
         </div>
     );
 };
